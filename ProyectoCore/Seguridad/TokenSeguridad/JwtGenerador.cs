45cf3e2b40b83b55ed20b8b156a5df9a421f278f
Lucas Olmos <lucolmos@gmail.com>
2020-05-16 13:00:25 -0300

    Creacion de token

--- ProyectoCore/Seguridad/TokenSeguridad/JwtGenerador.cs
+++ ProyectoCore/Seguridad/TokenSeguridad/JwtGenerador.cs
@@ -1,7 +1,10 @@
 ﻿using Aplicacion.Contratos;
 using Dominio;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Seguridad.TokenSeguridad
@@ -10,7 +13,26 @@ namespace Seguridad.TokenSeguridad
     {
         public string CrearToken(Usuario usuario)
         {
-            throw new NotImplementedException();
+            // informacion adicional que se le agrega al Token
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+            var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescripcion = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(30),
+                SigningCredentials = credenciales
+            };
+
+            var tokenManejador = new JwtSecurityTokenHandler();
+            var token = tokenManejador.CreateToken(tokenDescripcion);
+
+            return tokenManejador.WriteToken(token);
         }
     }
 }
