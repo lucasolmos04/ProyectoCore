04b0e12c7085ba1dd1c21559fa5c509ab8164b52
Lucas Olmos <lucolmos@gmail.com>
2020-07-10 19:35:02 -0300

    instalacion de router

--- ProyectoCore/cursos-online-app/src/App.js
+++ ProyectoCore/cursos-online-app/src/App.js
@@ -5,12 +5,23 @@ import RegistrarUsuario from "./componentes/seguridad/RegistrarUsuario";
 import Login from "./componentes/seguridad/Login";
 import Perfil from "./componentes/seguridad/PerfilUsuario";
 import PerfilUsuario from "./componentes/seguridad/PerfilUsuario";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
 function App() {
   return (
-    <MuithemeProvider theme={theme}>
-      <Login />
-    </MuithemeProvider>
+    <Router>
+      <MuithemeProvider theme={theme}>
+        <Grid container>
+          <Switch>
+            <Route exact path="/auth/login" component={Login} />
+            <Route exact path="/auth/registrar" component={RegistrarUsuario} />
+            <Route exact path="/auth/perfil" component={PerfilUsuario} />
+            <Route exact path="/" component={PerfilUsuario} />
+          </Switch>
+        </Grid>
+      </MuithemeProvider>
+    </Router>
   );
 }
 
