66a71e86b567cedb5f3a56b0ab7c595c8aa7628b
Lucas Olmos <lucolmos@gmail.com>
2020-07-25 16:36:22 -0300

    Se crea migracion de la clase Documento

--- ProyectoCore/Dominio/Documento.cs
+++ ProyectoCore/Dominio/Documento.cs
@@ -6,6 +6,11 @@ namespace Dominio
 {
     public class Documento
     {
-
+        public Guid DocumentoId { get; set; }
+        public Guid ObjetoReferencia { get; set; }
+        public string Nombre { get; set; }
+        public string Extencion { get; set; }
+        public byte[] Contenido { get; set; }
+        public DateTime FechaCreacion { get; set; }
     }
 }
