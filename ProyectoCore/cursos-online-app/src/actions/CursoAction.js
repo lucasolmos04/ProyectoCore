108c514e69523a6720a0d7b30bd47d2c75b7237c
Lucas Olmos <lucolmos@gmail.com>
2020-07-30 19:03:39 -0300

    guardado de curso e imagen - vinculacion con backend

---
+++ ProyectoCore/cursos-online-app/src/actions/CursoAction.js
@@ -0,0 +1,14 @@
+import HttpCliente from "../servicios/HttpCliente";
+
+export const guardarCurso = async (curso, imagen) => {
+  const endPointCurso = "/cursos";
+  const promesaCurso = HttpCliente.post(endPointCurso, curso);
+
+  if (imagen) {
+    const endPointImagen = "/documento";
+    const promesaImagen = HttpCliente.post(endPointImagen, imagen);
+    return await Promise.all([promesaCurso, promesaImagen]);
+  } else {
+    return await Promise.all([promesaCurso]);
+  }
+};
--- ProyectoCore/cursos-online-app/src/componentes/cursos/NuevoCurso.js
+++ ProyectoCore/cursos-online-app/src/componentes/cursos/NuevoCurso.js
@@ -15,16 +15,30 @@ import DateFnsUtils from "@date-io/date-fns"; // Da soporte a las fechas
 import ImageUploader from "react-images-upload";
 import { v4 as uuidv4 } from "uuid";
 import { obtenerDataImagen } from "../../actions/ImagenAction";
+import { guardarCurso } from "../../actions/CursoAction";
+import { useStateValue } from "../../contexto/store";
 
 const NuevoCurso = () => {
+  const [{ sesionUsuario }, dispatch] = useStateValue();
   const [fechaSeleccionada, setFechaSeleccionada] = useState(new Date());
-  const [imageCurso, setImagenCurso] = useState(null);
+  const [imagenCurso, setImagenCurso] = useState(null);
   const [curso, setCurso] = useState({
     titulo: "",
     descripcion: "",
     precio: 0.0,
     promocion: 0.0,
   });
+
+  const resetearForm = () => {
+    setFechaSeleccionada(new Date());
+    setImagenCurso(null);
+    setCurso({
+      titulo: "",
+      descripcion: "",
+      precio: 0.0,
+      promocion: 0.0,
+    });
+  };
 
   const ingresarValoresMemoria = (e) => {
     const { name, value } = e.target; // Obtiene los valores de la caja de texo (name y value)
@@ -39,6 +53,61 @@ const NuevoCurso = () => {
     const foto = imagenes[0];
     obtenerDataImagen(foto).then((respuesta) => {
       setImagenCurso(respuesta);
+    });
+  };
+
+  const guardarCursoBoton = (e) => {
+    e.preventDefault();
+
+    const cursoId = uuidv4();
+
+    const objetoCurso = {
+      titulo: curso.titulo,
+      descripcion: curso.descripcion,
+      promocion: parseFloat(curso.promocion || 0.0),
+      precio: parseFloat(curso.precio || 0.0),
+      fechaPublicacion: fechaSeleccionada,
+      cursoId: cursoId,
+    };
+
+    let objetoImagen = null;
+    if (imagenCurso) {
+      objetoImagen = {
+        nombre: imagenCurso.nombre,
+        data: imagenCurso.data,
+        extension: imagenCurso.extension,
+        objetoReferencia: cursoId,
+      };
+    }
+
+    guardarCurso(objetoCurso, objetoImagen).then((respuestas) => {
+      const responseCurso = respuestas[0];
+      const responseImagen = respuestas[1];
+      let mensaje = "";
+
+      if (responseCurso.status === 200) {
+        mensaje += "Se guardo exitosamente el curso";
+        resetearForm();
+      } else {
+        mensaje += "Errores: " + Object.keys(responseCurso.data.errors);
+      }
+
+      if (responseImagen) {
+        if (responseImagen.status === 200) {
+          mensaje += ", se guardo la imagen correctamente";
+        } else {
+          mensaje +=
+            "Errores en Imagen: " + Object.keys(responseImagen.data.errors);
+        }
+      }
+
+      dispatch({
+        type: "OPEN_SNACKBAR",
+        openMensaje: {
+          open: true,
+          mensaje: mensaje,
+        },
+      });
     });
   };
 
@@ -128,6 +197,7 @@ const NuevoCurso = () => {
                 variant="contained"
                 color="primary"
                 style={style.submit}
+                onClick={guardarCursoBoton}
               >
                 Guardar Curso
               </Button>
